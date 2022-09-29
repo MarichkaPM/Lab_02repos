df862c9f0ca0b41510fb2c3cfd6307ce457bdbf8
MarichkaPM <tanechnyk.marichka@gmail.com>
2022-09-29 21:45:03 +0300

    first branch_02 commit

--- lab_03/lab_03.cpp
+++ lab_03/lab_03.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-	cout << "Hello, World!" << endl;
-	int p = 1;
-	int i = 1;
-	while (i <= 10)
-	{
-		p *= i;
-		i++;
-	};
-	cout << "p = " << p << " i = " << i << endl;
-	system("pause");
+	double x; // вхідний параметр
+	// double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "x = "; cin >> x;
+	// z1 = 2 * x;
+	z2 = x + x;
+	cout << endl;
+	// cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
 	return 0;
 }
